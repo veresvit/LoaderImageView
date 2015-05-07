bcc53bb690113d6c05b0dda11ebf5f6e68cd1691
Vit Veres <veres.vit@gmail.com>
2015-05-07 20:06:52 +0200

    Renamed packages

--- LoaderImageView/src/androidTest/java/com/veresvit/example/ApplicationTest.java
+++ LoaderImageView/src/androidTest/java/com/veresvit/example/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.veresvit.loaderimageview;
+package com.veresvit.example;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ LoaderImageView/src/main/java/com/veresvit/loaderimageview/LoaderImageView.java
@@ -0,0 +1,23 @@
+package com.veresvit.loaderimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Vit Veres on 5/7/2015.
+ */
+public class LoaderImageView extends ImageView {
+	public LoaderImageView(Context context) {
+		super(context);
+	}
+
+	public LoaderImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public LoaderImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+}
