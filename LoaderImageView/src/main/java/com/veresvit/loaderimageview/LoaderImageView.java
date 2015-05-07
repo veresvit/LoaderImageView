2607fbb9bd51a09089815e21916b71a6c855c5b9
Vit Veres <veres.vit@gmail.com>
2015-05-07 23:32:47 +0200

    Added license

--- LoaderImageView/src/main/java/com/veresvit/loaderimageview/LoaderImageView.java
+++ LoaderImageView/src/main/java/com/veresvit/loaderimageview/LoaderImageView.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Vit Veres
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.veresvit.loaderimageview;
 
 import android.content.Context;
@@ -8,16 +32,22 @@ import android.widget.ImageView;
  * Created by Vit Veres on 5/7/2015.
  */
 public class LoaderImageView extends ImageView {
+
+	private Context mContext;
+
 	public LoaderImageView(Context context) {
 		super(context);
+		this.mContext = context;
 	}
 
 	public LoaderImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		this.mContext = context;
 	}
 
 	public LoaderImageView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		this.mContext = context;
 	}
 
 }
