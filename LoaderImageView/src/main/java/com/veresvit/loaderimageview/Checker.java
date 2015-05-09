561e733b66855687f3f5ad755da1ab73fcc35760
Vit Veres <veres.vit@gmail.com>
2015-05-09 19:52:12 +0200

    Refactoring, added comments to the code

--- LoaderImageView/src/main/java/com/veresvit/loaderimageview/Checker.java
+++ LoaderImageView/src/main/java/com/veresvit/loaderimageview/Checker.java
@@ -4,13 +4,28 @@
 
 package com.veresvit.loaderimageview;
 
+/**
+ * Contains checks of the attributes.
+ */
 class Checker {
 
+	/**
+	 * Checks whether the input string is not null and has at least length 1.
+	 *
+	 * @param input Input string.
+	 * @return True when the string is not null and length > 0, false otherwise.
+	 */
 	static boolean isUsed(String input) {
 		return input != null && input.length() > 0;
 	}
 
+	/**
+	 * Checks whether the input resource ID is valid.
+	 *
+	 * @param input Resource ID.
+	 * @return True when ID is different from zero, false otherwise.
+	 */
 	static boolean isUsed(int input) {
-		return input > 0;
+		return input != 0;
 	}
 }
--- LoaderImageView/src/main/java/com/veresvit/loaderimageview/LoaderImageView.java
+++ LoaderImageView/src/main/java/com/veresvit/loaderimageview/LoaderImageView.java
@@ -12,6 +12,9 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
 
+/**
+ * ImageView extension with support for async loading of images from an URI, with ability to set a placeholder or an error image.
+ */
 public class LoaderImageView extends ImageView {
 
 	public static final String TAG = LoaderImageView.class.getSimpleName();
@@ -24,14 +27,32 @@ public class LoaderImageView extends ImageView {
 	private int mPlaceholder;
 	private int mError;
 
+	/**
+	 * Basic constructor.
+	 *
+	 * @param context Inflater.
+	 */
 	public LoaderImageView(Context context) {
 		this(context, null);
 	}
 
+	/**
+	 * Constructor with attributes from XML.
+	 *
+	 * @param context Inflater.
+	 * @param attrs   View attributes such as uri or placeholder.
+	 */
 	public LoaderImageView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
 
+	/**
+	 * Constructor with attributes from XML and style.
+	 *
+	 * @param context      Inflater.
+	 * @param attrs        View attributes such as uri or placeholder.
+	 * @param defStyleAttr Default style of the view.
+	 */
 	public LoaderImageView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		this.mContext = context;
@@ -39,12 +60,25 @@ public class LoaderImageView extends ImageView {
 		load();
 	}
 
+	/**
+	 * Checks whether the URI attribute is set. Loads the image if yes.
+	 */
 	public void load() {
 		if (Checker.isUsed(mUri)) {
 			createRequest();
 		}
 	}
 
+	/**
+	 * Invalidates cache for the currently loaded URI.
+	 */
+	public void invalidateCache() {
+		Picasso.with(mContext).invalidate(mUri);
+	}
+
+	/**
+	 * Creates Picasso request with the given URI. Sets the given attributes.
+	 */
 	private void createRequest() {
 		RequestCreator requestCreator = Picasso.with(mContext).load(mUri);
 
@@ -56,6 +90,11 @@ public class LoaderImageView extends ImageView {
 		requestCreator.into(this);
 	}
 
+	/**
+	 * Parses set attributes.
+	 *
+	 * @param attrs Attributes from XML.
+	 */
 	private void parseAttributes(AttributeSet attrs) {
 		TypedArray typedAttributes = mContext.obtainStyledAttributes(attrs, R.styleable.LoaderImageView);
 
@@ -66,36 +105,66 @@ public class LoaderImageView extends ImageView {
 			if (attrId == R.styleable.LoaderImageView_uri) {
 				mUri = typedAttributes.getString(attrId);
 			} else if (attrId == R.styleable.LoaderImageView_placeholder) {
-				mPlaceholder = typedAttributes.getResourceId(attrId, -1);
+				mPlaceholder = typedAttributes.getResourceId(attrId, 0);
 			} else if (attrId == R.styleable.LoaderImageView_error) {
-				mError = typedAttributes.getResourceId(attrId, -1);
+				mError = typedAttributes.getResourceId(attrId, 0);
 			}
 		}
 
 		typedAttributes.recycle();
 	}
 
+	/**
+	 * Gets current URI.
+	 *
+	 * @return Current URI.
+	 */
 	public String getUri() {
 		return mUri;
 	}
 
-	public void setUri(String mUri) {
-		this.mUri = mUri;
+	/**
+	 * Sets a new URI.
+	 *
+	 * @param uri String URI to set.
+	 */
+	public void setUri(String uri) {
+		this.mUri = uri;
 	}
 
+	/**
+	 * Gets current placeholder.
+	 *
+	 * @return Placeholder resource ID.
+	 */
 	public int getPlaceholder() {
 		return mPlaceholder;
 	}
 
-	public void setPlaceholder(int mPlaceholder) {
-		this.mPlaceholder = mPlaceholder;
+	/**
+	 * Sets a new placeholder.
+	 *
+	 * @param placeholderResource Placeholder resource. Example: R.drawable.placeholder
+	 */
+	public void setPlaceholder(int placeholderResource) {
+		this.mPlaceholder = placeholderResource;
 	}
 
+	/**
+	 * Gets current loading error.
+	 *
+	 * @return Loading error resource ID.
+	 */
 	public int getError() {
 		return mError;
 	}
 
-	public void setError(int mError) {
-		this.mError = mError;
+	/**
+	 * Sets a new loading error.
+	 *
+	 * @param errorResource Loading error resource. Example: R.drawable.error
+	 */
+	public void setError(int errorResource) {
+		this.mError = errorResource;
 	}
 }
