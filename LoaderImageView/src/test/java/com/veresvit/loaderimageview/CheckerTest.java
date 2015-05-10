1f387bf4c163a8d5dc99be9e610643c9a1da9594
Vit Veres <veres.vit@gmail.com>
2015-05-10 21:03:41 +0200

    Implemented support for JUnit tests with Robolectric

---
+++ LoaderImageView/src/test/java/com/veresvit/loaderimageview/CheckerTest.java
@@ -0,0 +1,25 @@
+/*
+ * See the file "LICENSE" for the full license governing this code.
+ */
+
+package com.veresvit.loaderimageview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class CheckerTest {
+
+	@Test
+	public void testIsUsed() throws Exception {
+
+	}
+
+	@Test
+	public void testIsUsed1() throws Exception {
+
+	}
+}
---
+++ LoaderImageView/src/test/java/com/veresvit/loaderimageview/LoaderImageViewTest.java
@@ -0,0 +1,45 @@
+/*
+ * See the file "LICENSE" for the full license governing this code.
+ */
+
+package com.veresvit.loaderimageview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class LoaderImageViewTest {
+
+	@Test
+	public void testGetUri() throws Exception {
+
+	}
+
+	@Test
+	public void testSetUri() throws Exception {
+
+	}
+
+	@Test
+	public void testGetPlaceholder() throws Exception {
+
+	}
+
+	@Test
+	public void testSetPlaceholder() throws Exception {
+
+	}
+
+	@Test
+	public void testGetError() throws Exception {
+
+	}
+
+	@Test
+	public void testSetError() throws Exception {
+
+	}
+}
